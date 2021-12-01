65d5d1a04f4ba912fe59188e8f173b76b1c6f39c
iamsaikiran <39546922+iamsaikiran@users.noreply.github.com>
2021-11-30 19:32:29 -0600

    created an application

--- routes/mazda.js
+++ routes/mazda.js
@@ -1,12 +1,20 @@
 var express = require('express'); 
 const mazda_controlers= require('../controllers/mazda'); 
 var router = express.Router(); 
+
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET mazdas */ 
 router.get('/', mazda_controlers.mazda_view_all_Page );
 router.get('/detail', mazda_controlers.mazda_view_one_Page);
 router.get('/create', mazda_controlers.mazda_create_Page);
-router.get('/update', mazda_controlers.mazda_update_Page);
+router.get('/update', secured, mazda_controlers.mazda_update_Page);
 router.get('/delete', mazda_controlers.mazda_delete_Page);
 
 module.exports = router;
